e6b8d8ec9f76996589ae48d03b3b64ddd3d9d795
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-05 18:00:31 +0100

    Mouse detection fct

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,7 @@ int main(int ac, char **av)
 	sfRenderWindow	*wd;
 	sfVideoMode	video_md;
 	sfTime		tm;
+	int i = 0;
 
 	tm.microseconds = 2000;
 	video_md.width = 1920;
@@ -21,6 +22,10 @@ int main(int ac, char **av)
 	button_t *btn = create_button(100, 100);
 	while (sfRenderWindow_isOpen(wd)) {
 		btn->draw(btn, wd);
+		if (btn->collide(btn, sfMouse_getPositionRenderWindow(wd).x, sfMouse_getPositionRenderWindow(wd).y) == 1) {
+			printf("Oooh clique %d\n", i);
+			++i;
+		}
 		sfRenderWindow_display(wd);
 		sfSleep(tm);
 	}
