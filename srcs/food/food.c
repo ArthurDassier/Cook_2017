38d852e647b3bfcb8259f86e2384d7765851d445
Leo Smith <k0oo@icloud.com>
2018-02-11 16:14:56 +0100

    added generator

---
+++ srcs/food/food.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2017
+** food.c
+** File description:
+** food generator file
+*/
+
+#include "my_cook.h"
+
+static struct queue *grilled_bug(void)
+{
+	struct queue	*tmp = NULL;
+
+	return (tmp);
+}
+
+struct queue *generate_food(void)
+{
+	struct queue	*tmp = NULL;
+	struct queue	*(*tab[])(void) = { &grilled_bug, NULL };
+	int		no = 0;
+
+	tmp = tab[no]();
+	if (tmp == NULL)
+		return (NULL);
+	return (tmp);
+}
