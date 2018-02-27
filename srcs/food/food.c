176c8c5b73fc05095fa79a4cd96eca4ac13b3a6b
Arthur Dassier <arthur.dassier@epitech.eu>
2018-02-27 17:37:47 +0100

    added generate food tab

--- srcs/food/food.c
+++ srcs/food/food.c
@@ -11,15 +11,44 @@ static struct queue *grilled_bug(void)
 {
 	struct queue	*tmp = NULL;
 
+	add_queue(tmp, create_food(0, 0, GRILLED_BUG));
+	return (tmp);
+}
+
+static struct queue *spider_and_worm(void)
+{
+	struct queue	*tmp = NULL;
+
+	add_queue(tmp, create_food(0, 0, COOKED_SPIDER));
+	add_queue(tmp, create_food(0, 0, COOKED_WORM));
+	return (tmp);
+}
+
+static struct queue *bug_and_worm(void)
+{
+	struct queue	*tmp = NULL;
+
+	add_queue(tmp, create_food(0, 0, GRILLED_BUG));
+	add_queue(tmp, create_food(0, 0, COOKED_WORM));
+	return (tmp);
+}
+
+static struct queue *worm(void)
+{
+	struct queue	*tmp = NULL;
+
+	add_queue(tmp, create_food(0, 0, COOKED_WORM));
 	return (tmp);
 }
 
 struct queue *generate_food(void)
 {
 	struct queue	*tmp = NULL;
-	struct queue	*(*tab[])(void) = {&grilled_bug, NULL};
-	int		no = 0;
+	struct queue	*(*tab[5])(void) = {&grilled_bug, &spider_and_worm,
+		 			&bug_and_worm, &worm, NULL};
 
+	srand(time(NULL));
+	no = rand() % 3;
 	tmp = tab[no]();
 	if (tmp == NULL)
 		return (NULL);
